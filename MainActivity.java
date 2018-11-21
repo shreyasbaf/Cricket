a9ddefcc31a4fff7b39f22085d30c53547e723d1
Shreyas Bafna <33115156+shreyasbaf@users.noreply.github.com>
2018-11-21 18:43:02 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.hp_pc.cricket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static WebView mWebView;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.loadUrl("https://shreyasbafna7.wordpress.com");
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+
+    }
+
+
+}
